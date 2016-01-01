0ff59f5b263b16ba5b6ef5eb89585da19f88f203
Szikszai Gusztáv <guszti5@hotmail.com>
2016-01-01 05:45:14 +0100

    Added Calendar disabled & readonly specs.

--- specs/Ui/Calendar.js
+++ specs/Ui/Calendar.js
@@ -66,5 +66,53 @@ define([
           .findByCssSelector('ui-calendar-cell:nth-child(1):not(.selected)')
       })
     })
+
+    bdd.describe('Readonly', function() {
+      var calendar;
+
+      bdd.beforeEach(function() {
+        calendar = helpers
+          .getElement(this.remote, 'td:nth-child(2) ui-calendar')
+      })
+
+      bdd.it("should not display chevrons", function(){
+        return calendar
+          .findByCssSelector('.ion-chevron-left')
+          .isDisplayed(assert.isFalse)
+          .end()
+          .findByCssSelector('.ion-chevron-right')
+          .isDisplayed(assert.isFalse)
+      })
+
+      bdd.it("cells should not be clickable", function(){
+        return calendar
+          .findByCssSelector('ui-calendar-cell:nth-child(6)')
+          .getProperty('onmousedown').then(assert.isNull)
+      })
+    })
+
+    bdd.describe('Disabled', function() {
+      var calendar;
+
+      bdd.beforeEach(function() {
+        calendar = helpers
+          .getElement(this.remote, 'td:nth-child(3) ui-calendar.disabled')
+      })
+
+      bdd.it("chevrons should not be clickable", function(){
+        return calendar
+          .findByCssSelector('.ion-chevron-left')
+          .getProperty('onmousedown').then(assert.isNull)
+          .end()
+          .findByCssSelector('.ion-chevron-right')
+          .getProperty('onmousedown').then(assert.isNull)
+      })
+
+      bdd.it("cells should not be clickable", function(){
+        return calendar
+          .findByCssSelector('ui-calendar-cell:nth-child(6)')
+          .getProperty('onmousedown').then(assert.isNull)
+      })
+    })
   })
 })

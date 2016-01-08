3591e0ec1d94f49e610ab4504e9c84f144cf774e
Szikszai Gusztáv <guszti5@hotmail.com>
2016-01-08 08:11:15 +0100

    Remove fxNone function from Main.

--- source/Main.elm
+++ source/Main.elm
@@ -389,10 +389,6 @@ view address model =
         ]
       ]
 
-fxNone : Model -> (Model, Effects.Effects Action)
-fxNone model =
-  (model, Effects.none)
-
 update : Action -> Model -> Model
 update action model =
   case action of
@@ -491,8 +487,7 @@ update' action model =
       in
         ({ model | notifications = notis }, Effects.map Notis effect)
     _ ->
-      update action model
-        |> fxNone
+      (update action model, Effects.none)
 
 app =
   let

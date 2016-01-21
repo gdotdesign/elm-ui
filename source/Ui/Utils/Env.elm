80c6d18b0acdca65842eb752849b185707dd1dcb
Szikszai Gusztáv <guszti5@hotmail.com>
2016-01-21 07:24:54 +0100

    Revert evn changes.

--- source/Ui/Utils/Env.elm
+++ source/Ui/Utils/Env.elm
@@ -2,26 +2,13 @@ module Ui.Utils.Env where
 
 {-| Module for interacting with the environment variables.
 
-@docs get, debug, log
+@docs get
 -}
 import Date.Format exposing (format)
 import Json.Decode as Json
 import Native.Env
 import Ext.Date
 import Debug
-
-{-| Gets and saves the debug key's value into this function. -}
-debug : Bool
-debug =
-  get "debug" False Json.bool
-
-{-| Logs a mesage if the debug key is True. -}
-log : String -> String
-log message =
-  if debug then
-    Debug.log (format "%H:%M:%S" (Ext.Date.now ())) message
-  else
-    ""
 
 {-| Gets the value of the given environment variable with a decoder and a
 default value. -}

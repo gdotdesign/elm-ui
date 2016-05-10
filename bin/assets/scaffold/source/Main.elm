d1f9f4a0879e014763376e5bc477f66d5ee2e2d5
Szikszai Gusztáv <guszti5@hotmail.com>
2016-05-10 16:30:09 +0200

    Update scaffolded app.

--- bin/assets/scaffold/source/Main.elm
+++ bin/assets/scaffold/source/Main.elm
@@ -1,26 +1,25 @@
-module Main where
-
-import Signal exposing (forwardTo)
-import StartApp
-import Effects
-import Task
+module Main exposing (..)
 
 import Html exposing (div, span, strong, text)
+import Html.App
 
 import Ui.Container
 import Ui.Button
 import Ui.App
 import Ui
 
+
 type alias Model =
   { app : Ui.App.Model
   , counter : Int
   }
 
-type Action
-  = App Ui.App.Action
+
+type Msg
+  = App Ui.App.Msg
   | Increment
   | Decrement
+
 
 init : Model
 init =
@@ -28,48 +27,51 @@ init =
   , counter = 0
   }
 
-update : Action -> Model -> (Model, Effects.Effects Action)
-update action model =
-  case action of
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+  case msg of
     App act ->
       let
-        (app, effect) = Ui.App.update act model.app
+        ( app, effect ) =
+          Ui.App.update act model.app
       in
-        ({ model | app = app }, Effects.map App effect)
+        ( { model | app = app }, Cmd.map App effect )
 
     Increment ->
-      ({ model | counter = model.counter + 1 }, Effects.none)
+      ( { model | counter = model.counter + 1 }, Cmd.none )
 
     Decrement ->
-      ({ model | counter = model.counter - 1 }, Effects.none)
+      ( { model | counter = model.counter - 1 }, Cmd.none )
 
-view : Signal.Address Action -> Model -> Html.Html
-view address model =
-  Ui.App.view (forwardTo address App) model.app
-    [ Ui.Container.column []
-      [ Ui.title [] [text "Elm-UI Counter"]
-      , Ui.textBlock "This is an minimal project to get you started with Elm-UI!"
-      , div []
-        [ span [] [text "Counter:"]
-        , strong [] [text (toString model.counter)]
+
+view : Model -> Html.Html Msg
+view model =
+  Ui.App.view
+    App
+    model.app
+    [ Ui.Container.column
+        []
+        [ Ui.title [] [ text "Elm-UI Counter" ]
+        , Ui.textBlock "This is an minimal project to get you started with Elm-UI!"
+        , div
+            []
+            [ span [] [ text "Counter:" ]
+            , strong [] [ text (toString model.counter) ]
+            ]
+        , Ui.Container.row
+            []
+            [ Ui.Button.primary "Decrement" Decrement
+            , Ui.Button.primary "Increment" Increment
+            ]
         ]
-      , Ui.Container.row []
-        [ Ui.Button.primary "Decrement" address Decrement
-        , Ui.Button.primary "Increment" address Increment
-        ]
-      ]
     ]
 
-app =
-  StartApp.start { init = (init, Effects.none)
-                 , update = update
-                 , view = view
-                 , inputs = []
-                 }
 
 main =
-  app.html
-
-port tasks : Signal (Task.Task Effects.Never ())
-port tasks =
-  app.tasks
+  Html.App.program
+    { init = ( init, Cmd.none )
+    , view = view
+    , update = update
+    , subscriptions = \_ -> Sub.none
+    }

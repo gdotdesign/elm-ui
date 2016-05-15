b2f3838475e049d169318f71a1151dfdc7541f96
Szikszai Gusztáv <guszti5@hotmail.com>
2016-05-15 10:50:12 +0200

    Finalize Ui.Layout.

--- source/Ui/Layout.elm
+++ source/Ui/Layout.elm
@@ -1,30 +1,55 @@
 module Ui.Layout exposing (..)
 
+{-| Module that provides most commonly used layouts.
+
+@docs sidebar, default, centerDefault
+-}
+
 import Html.Attributes exposing (class)
 import Html exposing (node)
 
 import Ui.Header
 
-sidebar : Int -> List (Html.Html msg) -> List (Html.Html msg) -> Html.Html msg
-sidebar size sidebarContent mainContent =
-  node "ui-layout" [class "ui-layout-sidebar"]
+
+{-| Layout with a sidebar on the left side.
+
+    Ui.Layout.sidebar sidebarContent mainContent
+-}
+sidebar : List (Html.Html msg) -> List (Html.Html msg) -> Html.Html msg
+sidebar sidebarContent mainContent =
+  node
+    "ui-layout"
+    [ class "ui-layout-sidebar" ]
     [ node "ui-layout-sidebar" [] sidebarContent
     , node "ui-layout-main" [] mainContent
     ]
 
 
+{-| Layout with header, content and footer, where the content fills the empty
+space (sticky footer).
+
+    Ui.Layout.default headerContent mainContent footerContent
+-}
 default : List (Html.Html msg) -> List (Html.Html msg) -> List (Html.Html msg) -> Html.Html msg
 default headerContent mainContent footerContent =
-  node "ui-layout" [class "ui-layout-default"]
+  node
+    "ui-layout"
+    [ class "ui-layout-default" ]
     [ Ui.Header.view [] headerContent
     , node "ui-layout-main" [] mainContent
     , node "ui-layout-footer" [] footerContent
     ]
 
 
+{-| Same as the default layout but content is centered in all three parts.
+
+    Ui.Layout.centerDefault headerContent mainContent footerContent
+-}
 centerDefault : List (Html.Html msg) -> List (Html.Html msg) -> List (Html.Html msg) -> Html.Html msg
 centerDefault headerContent mainContent footerContent =
-  node "ui-layout" [class "ui-layout-center-default"]
+  node
+    "ui-layout"
+    [ class "ui-layout-center-default" ]
     [ Ui.Header.view [] [ node "ui-layout-center" [] headerContent ]
     , node "ui-layout-main" [] [ node "ui-layout-center" [] mainContent ]
     , node "ui-layout-footer" [] [ node "ui-layout-center" [] footerContent ]

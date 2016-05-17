0b476350dbde366ff28fd63d05fbed1a3d6a2f71
Szikszai Gusztáv <guszti5@hotmail.com>
2016-05-17 08:03:20 +0200

    Finalize Ui.NotificationCenter.

--- source/Ui/NotificationCenter.elm
+++ source/Ui/NotificationCenter.elm
@@ -1,5 +1,4 @@
-module Ui.NotificationCenter exposing
-  (Model, Msg, init, update, view, notify)
+module Ui.NotificationCenter exposing (Model, Msg, init, update, view, notify)
 
 {-| Notification center for displaying messages to the user.
 
@@ -27,6 +26,7 @@ import Task
 
 import Ui.Native.Browser as Browser
 
+
 {-| Representation of a notification center:
   - **timeout** - The timeout of the notification before it's hidden
   - **notifications** - The list of notifications that is displayed
@@ -38,17 +38,20 @@ type alias Model msg =
   , timeout : Float
   }
 
-{-| Actions that notification center can make. -}
+
+{-| Actions that notification center can make.
+-}
 type Msg
-  = AutoHide Int ()
-  | Remove Int ()
+  = AutoHide Int
+  | Remove Int
   | Hide Int
-  | Tasks ()
+  | NoOp
+
 
 {-| Initializes a notification center with the given timeout and duration (in
 milliseconds).
 
-    NotificationCenter.init timeout duration
+    notis = Ui.NotificationCenter.init timeout duration
 -}
 init : Float -> Float -> Model msg
 init timeout duration =
@@ -57,45 +60,59 @@ init timeout duration =
   , timeout = timeout
   }
 
-{-| Renders a notification center. -}
-view: (Msg -> a) -> Model a -> Html.Html a
-view address model =
-  render address model
 
-{-| Updates a notification center. -}
-update: Msg -> Model msg -> (Model msg, Cmd Msg)
+{-| Updates a notification center.
+
+    Ui.NotificationCenter.update notis
+-}
+update : Msg -> Model msg -> ( Model msg, Cmd Msg )
 update action model =
   case action of
-    AutoHide id _ ->
+    AutoHide id ->
       autoHide id model
 
-    Remove id _ ->
+    Remove id ->
       remove id model
 
     Hide id ->
       hide id model
 
-    Tasks _ ->
-      (model, Cmd.none)
+    NoOp ->
+      ( model, Cmd.none )
+
+
+{-| Renders a notification center.
+
+    Ui.NotificationCenter.view notis
+-}
+view : (Msg -> a) -> Model a -> Html.Html a
+view address model =
+  render address model
+
 
 {-| Adds a notification with the given html content.
 
-    NotificationCenter.notify (text "Hello") model
+    Ui.NotificationCenter.notify (text "Hello") model
 -}
-notify : Html.Html msg -> Model msg -> (Model msg, Cmd Msg)
+notify : Html.Html msg -> Model msg -> ( Model msg, Cmd Msg )
 notify contents model =
   let
     notification =
       initNotification model contents
 
     updatedModel =
-      { model | notifications = model.notifications ++ [notification] }
+      { model | notifications = model.notifications ++ [ notification ] }
   in
-    (updatedModel, Task.perform Tasks (AutoHide notification.id) (Process.sleep model.timeout))
+    ( updatedModel
+    , performTask (AutoHide notification.id) (Process.sleep model.timeout) )
 
--- PRIVATE --
 
--- Represents a notification
+
+----------------------------------- PRIVATE ------------------------------------
+
+
+{-| Representation of a notification.
+-}
 type alias Notification msg =
   { contents : Html.Html msg
   , duration : Float
@@ -103,13 +120,24 @@ type alias Notification msg =
   , id : Int
   }
 
--- Render
-render: (Msg -> a) -> Model a -> Html.Html a
+
+performTask : Msg -> Task.Task a b -> Cmd Msg
+performTask msg task =
+  Task.perform (\_ -> NoOp) (\_ -> msg) task
+
+
+{-| Renders a notification center.
+-}
+render : (Msg -> a) -> Model a -> Html.Html a
 render address model =
-  node "ui-notification-center" []
+  node
+    "ui-notification-center"
+    []
     (List.map (renderNotification address) model.notifications)
 
--- Renders a notification
+
+{-| Renders a notification.
+-}
 renderNotification : (Msg -> a) -> Notification a -> Html.Html a
 renderNotification address model =
   let
@@ -118,30 +146,43 @@ renderNotification address model =
 
     prefix =
       case Browser.prefix of
-        Browser.Webkit -> "-webkit-"
-        Browser.Moz -> "-moz-"
-        Browser.MS -> "-ms-"
-        Browser.O -> "-o-"
-        _ -> ""
+        Browser.Webkit ->
+          "-webkit-"
+
+        Browser.Moz ->
+          "-moz-"
+
+        Browser.MS ->
+          "-ms-"
+
+        Browser.O ->
+          "-o-"
+
+        _ ->
+          ""
   in
-    node "ui-notification"
+    node
+      "ui-notification"
       [ id (toString model.id)
-      , classList [(model.class, True)]
+      , classList [ ( model.class, True ) ]
       , onClick (address (Hide model.id))
-      , style [ ("animation-duration", duration)
-              , (prefix ++ "animation-duration", duration)
-              ]
+      , style
+          [ ( "animation-duration", duration )
+          , ( prefix ++ "animation-duration", duration )
+          ]
       ]
       [ node "div" [] [ model.contents ] ]
 
--- Initiliazes a notification with the given contents and model
+
+{-| Initiliazes a notification with the given contents and model.
+-}
 initNotification : Model a -> Html.Html a -> Notification a
 initNotification model contents =
   let
     id =
       List.map .id model.notifications
-      |> List.maximum
-      |> Maybe.withDefault -1
+        |> List.maximum
+        |> Maybe.withDefault -1
   in
     { class = "ui-notification-show"
     , duration = model.duration
@@ -149,8 +190,10 @@ initNotification model contents =
     , id = id + 1
     }
 
--- Hides the notification with the given id
-hide : Int -> Model msg -> (Model msg, Cmd Msg)
+
+{-| Hides the notification with the given id.
+-}
+hide : Int -> Model msg -> ( Model msg, Cmd Msg )
 hide id model =
   let
     updatedNotifications =
@@ -159,13 +202,17 @@ hide id model =
     updatedNotification item =
       if item.id == id then
         { item | class = "ui-notification-hide" }
-      else item
+      else
+        item
   in
-    ({ model | notifications = updatedNotifications }
-     , Task.perform Tasks (Remove id) (Process.sleep (model.duration + 100)))
+    ( { model | notifications = updatedNotifications }
+    , performTask (Remove id) (Process.sleep (model.duration + 100))
+    )
 
--- Tries to hide the notification with the given id
-autoHide: Int -> Model a -> (Model a, Cmd Msg)
+
+{-| Tries to hide the notification with the given id.
+-}
+autoHide : Int -> Model a -> ( Model a, Cmd Msg )
 autoHide id model =
   let
     updatedNotifications =
@@ -173,35 +220,40 @@ autoHide id model =
 
     isMember =
       List.map .id model.notifications
-      |> List.member id
+        |> List.member id
 
     hideEffect =
       if updatedNotifications /= model.notifications then
-        Task.perform Tasks (Remove id) (Process.sleep (model.duration + 100))
+        performTask (Remove id) (Process.sleep (model.duration + 100))
       else
-        Task.perform Tasks (AutoHide id) (Process.sleep 100)
+        performTask (AutoHide id) (Process.sleep 100)
 
     effect =
-      if isMember then hideEffect else Cmd.none
+      if isMember then
+        hideEffect
+      else
+        Cmd.none
 
     updatedNotification item =
       let
         index =
           List.Extra.elemIndex item model.notifications
-          |> Maybe.withDefault -1
+            |> Maybe.withDefault -1
       in
         if item.id == id && index == 0 then
           { item | class = "ui-notification-hide" }
-        else item
+        else
+          item
   in
-    ({ model | notifications = updatedNotifications }, effect)
+    ( { model | notifications = updatedNotifications }, effect )
 
--- Removes the notification with the given id
-remove : Int -> Model a -> (Model a, Cmd Msg)
+
+{-| Removes the notification with the given id.
+-}
+remove : Int -> Model a -> ( Model a, Cmd Msg )
 remove id model =
   let
     updatedNotifications =
       List.filter (\item -> item.id /= id) model.notifications
   in
-    ({ model | notifications = updatedNotifications }, Cmd.none)
-
+    ( { model | notifications = updatedNotifications }, Cmd.none )

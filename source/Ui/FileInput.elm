ce7caf006394d8a7dfdb687be1f22aa73c6f3513
Gusztáv Szikszai <guszti5@hotmail.com>
2016-06-26 14:47:19 +0200

    Document elm files.

--- source/Ui/FileInput.elm
+++ source/Ui/FileInput.elm
@@ -1,28 +1,52 @@
-module Ui.FileInput exposing (..)
+module Ui.FileInput exposing
+  (Model, Msg, init, update, view, render)
 
+{-| Component for selecting a file.
+
+# Model
+@docs Model, Msg, init, update
+
+# View
+@docs view, render
+-}
+
+import Numeral exposing (format)
 import Task
+import Http
 
 import Html exposing (node, div, text)
 import Html.Lazy
 
-import Numeral exposing (format)
-import Http
-
 import Ui.Native.FileManager as FileManager exposing (File)
 import Ui.Button
 
+
+{-| Representation of a file input:
+  - **readonly** - Whether or not the date picker is readonly
+  - **disabled** - Whether or not the date picker is disabled
+  - **accept** - The mime types that the file input accepts
+  - **file** - (Maybe) The selected file
+-}
 type alias Model =
   { readonly : Bool
   , disabled : Bool
-  , file: Maybe File
+  , file : Maybe File
   , accept : String
   }
 
+
+{-| Messages that a file input can receive.
+-}
 type Msg
   = Browse
   | Selected File
   | Clear
 
+
+{-| Initializes a file input with the given accept value.
+
+    fileInput = Ui.FileInput.init "image/*"
+-}
 init : String -> Model
 init accept =
   { readonly = False
@@ -31,45 +55,58 @@ init accept =
   , file = Nothing
   }
 
-update : Msg -> Model -> (Model, Cmd Msg)
+
+{-| Updates a file input.
+
+    Ui.FileInput.update msg fileInput
+-}
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
   case msg of
     Browse ->
-      (model, Task.perform (\_ -> Debug.crash "") Selected (FileManager.open model.accept))
+      ( model, Task.perform (\_ -> Debug.crash "") Selected (FileManager.open model.accept) )
 
     Selected file ->
-      ({ model | file = Just file }, Cmd.none)
+      ( { model | file = Just file }, Cmd.none )
 
     Clear ->
-      ({ model | file = Nothing }, Cmd.none)
+      ( { model | file = Nothing }, Cmd.none )
 
-asFormData : String -> Model -> Http.Data
-asFormData key model =
-  case model.file of
-    Just file ->
-      FileManager.toFormData key file
-    Nothing ->
-      Debug.crash "There is no file selected!"
 
+{-| Renders a file input lazily.
+
+    Ui.FileInput.view fileInput
+-}
 view : Model -> Html.Html Msg
 view model =
   Html.Lazy.lazy render model
 
+
+{-| Renders a file input.
+
+    Ui.FileInput.render fileInput
+-}
 render : Model -> Html.Html Msg
 render model =
   node "ui-file-input"
     []
     [ renderFileStatus model
-    , Ui.Button.view Browse { text = "Browse"
-                            , kind = "primary"
-                            , readonly = model.readonly
-                            , disabled = model.disabled
-                            , size = "medium"
-                            }
+    , Ui.Button.view Browse
+        { text = "Browse"
+        , kind = "primary"
+        , readonly = model.readonly
+        , disabled = model.disabled
+        , size = "medium"
+        }
     ]
+
+
 
 ----------------------------------- PRIVATE ------------------------------------
 
+
+{-| Renders a file status for a file input.
+-}
 renderFileStatus : Model -> Html.Html Msg
 renderFileStatus model =
   case model.file of
@@ -80,6 +117,7 @@ renderFileStatus model =
         , node "ui-file-input-size" [] [ text ("Size: " ++ (format "0.0b" file.size)) ]
         , node "ui-file-input-type" [] [ text ("Type: " ++ file.mimeType) ]
         ]
+
     Nothing ->
       node "ui-file-input-no-file"
         []
--- source/Ui/Native/FileManager.elm
+++ source/Ui/Native/FileManager.elm
@@ -1,13 +1,36 @@
-module Ui.Native.FileManager exposing (..)
+module Ui.Native.FileManager exposing
+  (File, readAsString, readAsDataURL, toFormData, open, download)
 
-import Native.FileManager
+{-| Low level functions for managing [Files](https://developer.mozilla.org/en/docs/Web/API/File).
+
+# Reading
+@docs readAsString, readAsDataURL
+
+# For Http
+@docs toFormData
+
+# Open / Download
+@docs open, download
+-}
+
 import Http
 import Task exposing (Task)
 
+import Native.FileManager
+
+
 {-| The type of the files content.
 -}
-type Data = Data
+type Data
+  = Data
 
+
+{-| Representation of a file:
+  - **name** - The name of the file
+  - **mimeType** - The mime type of the file
+  - **size** - The size of the file
+  - **data** - The file data (native File object)
+-}
 type alias File =
   { name : String
   , mimeType : String
@@ -15,22 +38,47 @@ type alias File =
   , data : Data
   }
 
+
+{-| Reads a file as a string.
+
+    task = FileManager.readAsString file
+-}
 readAsString : File -> Task Never String
 readAsString file =
   Native.FileManager.readAsString file
 
+
+{-| Reads a file as a [data URI](https://en.wikipedia.org/wiki/Data_URI_scheme)
+
+    task = FileManager.readAsDataURL file
+-}
 readAsDataURL : File -> Task Never String
 readAsDataURL file =
   Native.FileManager.readAsDataURL file
 
+
+{-| Converts a files data to Http.Data
+
+    data = FileManager.toFormData file
+-}
 toFormData : String -> File -> Http.Data
 toFormData key file =
   Http.stringData key (Native.FileManager.toFormData file)
 
+
+{-| Opens a file browser for selecting a file.
+
+    task = FileManager.open "image/*"
+-}
 open : String -> Task Never File
 open accept =
   Native.FileManager.open accept
 
+
+{-| Downloads the given data with the given name and mime type.
+
+    task = FileManager.download "test.txt" "text/plain" "Hello World!"
+-}
 download : String -> String -> String -> Task Never String
 download filename mimeType data =
   Native.FileManager.download filename mimeType data

c0aa2468f44e69aa91e92d56895e01d139faf7ee
Szikszai Gusztáv <guszti5@hotmail.com>
2017-01-20 16:53:39 +0100

    Implement auto embedding of styles.

--- source/Native/Uid.js
+++ source/Native/Uid.js
@@ -15,8 +15,20 @@ var _gdotdesign$elm_ui$Native_Uid = function() {
     ].join('-')
   }
 
+  function getTags(memo, node) {
+    if(node.type === 'text') { return memo }
+    if(node.type === 'thunk') { node = node.thunk() }
+    if(!node) { return memo}
+    memo[node.tag] = true
+    node.children.reduce(getTags, memo)
+    return memo
+  }
+
   /* Interface */
   return {
+    getTags: function(a) {
+      return _elm_lang$core$Native_List.fromArray(Object.keys(getTags({},a)))
+    },
     uid: uid
   }
 }()
--- source/Ui/Styles.elm
+++ source/Ui/Styles.elm
@@ -2,7 +2,7 @@ module Ui.Styles exposing (..)
 
 {-| This module contains the styles for all components.
 
-@docs embed, embedSome, embedDefault
+@docs embed, embedSome, embedDefault, embedAuto, styles
 -}
 
 import Css.Properties exposing (..)
@@ -20,6 +20,16 @@ import Ui.Styles.Textarea as Textarea
 import Ui.Styles.Button as Button
 import Ui.Styles.Input as Input
 
+import Native.Uid
+
+{-|-}
+styles : Theme -> List (String, Node)
+styles theme =
+  [ ("ui-button-group", ButtonGroup.style theme )
+  , ("ui-calendar", Calendar.style theme )
+  , ("ui-button", Button.style theme )
+  , ("ui-container", Container.style )
+  ]
 
 {-| Renders the styles for the given components into a HTML tag.
 -}
@@ -33,6 +43,26 @@ embedSome nodes =
 embedDefault : Html.Html msg
 embedDefault =
   embed default
+
+
+{-|-}
+embedAuto : Html.Html msg -> Html.Html msg
+embedAuto input =
+  let
+    tags =
+      Native.Uid.getTags input
+
+    filteredStyles =
+      List.filterMap
+        (\(tag, style) ->
+          if List.member tag tags then
+            Just style
+          else
+            Nothing
+        )
+        (styles default)
+  in
+    embedSome filteredStyles
 
 
 {-| Renders the stylesheet with the given theme into an HTML tag.

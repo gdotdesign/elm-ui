e94e3d8c2fd3eba3b2cdc24edc9ce2de3045f312
Szikszai Gusztáv <guszti5@hotmail.com>
2017-02-16 16:19:39 +0100

    Convert breadcrumbs styles to the new variables system.

--- source/Ui/Styles/Breadcrumbs.elm
+++ source/Ui/Styles/Breadcrumbs.elm
@@ -2,61 +2,72 @@ module Ui.Styles.Breadcrumbs exposing (..)
 
 {-| Styles for breadcrumbs.
 
-@docs style, defaultStyle
+@docs style, variables
 -}
 import Css.Properties exposing (..)
 import Css exposing (..)
 
-import Ui.Styles.Theme as Theme exposing (Theme)
+import Ui.Styles.Theme as Theme exposing (default)
 import Ui.Styles.Mixins as Mixins
-import Ui.Styles exposing (Style)
+import Ui.Styles
 
-{-| Styles for breadcrumbs using the default theme.
+import Html
+
+{-| Variables for the styles of a button.
 -}
-defaultStyle : Style
-defaultStyle =
-  Ui.Styles.attributes (style Theme.default)
+variables : List (String, String)
+variables =
+  [ ("ui-breadcrumbs-background-color", default.breadcrumbs.background)
+  , ("ui-breadcrumbs-border-color", default.breadcrumbs.borderColor)
+  , ("ui-breadcrumbs-hover-color", default.colors.primary.color)
+  , ("ui-breadcrumbs-focus-color", default.colors.focus.color)
+  , ("ui-breadcrumbs-text-color", default.breadcrumbs.text)
+  , ("ui-breadcrumbs-font-family", default.fontFamily)
+  ]
+    |> Ui.Styles.registerVariables
 
 
 {-| Returns the style node for breadcrumbs using the given theme.
 -}
-style : Theme -> Node
-style theme =
-  mixin
-    [ Mixins.defaults
+style : List (Html.Attribute msg)
+style =
+  [ Mixins.defaults
 
-    , borderBottom ((px 1) . solid . theme.breadcrumbs.borderColor)
-    , background theme.breadcrumbs.background
-    , color theme.breadcrumbs.text
-    , fontFamily theme.fontFamily
-    , padding (px 15)
-    , display block
+  , borderBottom ((px 1) . solid . (var "ui-breadcrumbs-border-color"))
+  , background (var "ui-breadcrumbs-background-color")
+  , fontFamily (var "ui-breadcrumbs-font-family")
+  , color (var "ui-breadcrumbs-text-color")
+  , padding (px 15)
+  , display block
 
-    , selector "ui-breadcrumb"
-      [ display inlineBlock
+  , selector "ui-breadcrumb"
+    [ display inlineBlock
 
-      , selector "&:not([clickable])"
-        [ pointerEvents none
-        ]
-
-      , selector "a"
-        [ textDecoration none
-        , color currentColor
-        , cursor pointer
-        , outline none
-
-        , selector "&:hover"
-          [ color theme.colors.primary.color
-          ]
-
-        , selector "&:focus"
-          [ color theme.colors.focus.color
-          ]
-        ]
+    , selector "&:not([clickable])"
+      [ pointerEvents none
       ]
 
-    , selector "ui-breadcrumb-separator"
-      [ margin (zero . (px 10))
-      , opacity 0.5
+    , selector "a"
+      [ textDecoration none
+      , color currentColor
+      , cursor pointer
+      , outline none
+
+      , selector "&:hover"
+        [ color (var "ui-breadcrumbs-hover-color")
+        ]
+
+      , selector "&:focus"
+        [ color (var "ui-breadcrumbs-focus-color")
+        ]
       ]
     ]
+
+  , selector "ui-breadcrumb-separator"
+    [ margin (zero . (px 10))
+    , opacity 0.5
+    ]
+  ]
+  |> mixin
+  |> Ui.Styles.attributes
+  |> Ui.Styles.apply
